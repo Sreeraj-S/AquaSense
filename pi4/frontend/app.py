d7490152005ae4d56c113bb15858c9e5514a8596
Sreeraj S <sathishranji1@gmail.com>
2024-06-12 15:08:19 +0530

    Fix tables issue and fixed docker file

--- pi4/frontend/app.py
+++ pi4/frontend/app.py
@@ -1,6 +1,7 @@
 import mqtt
 from creation import create_app
-app = create_app()
+
 if __name__ == '__main__':
+    app = create_app()
     mqtt.mqtt_start()
     app.run(host="0.0.0.0")
--- pi4/frontend/creation.py
+++ pi4/frontend/creation.py
@@ -1,37 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+
 
 db = SQLAlchemy()
-migrate = Migrate()
 app = Flask(__name__)
 
 def create_app():
     app.config.from_object('config.Config')
-
+    from models import (
+    TopFillMessage,
+    BottomFillMessage,
+    AvailMessage,
+    MotorMessage
+)
     # Initialize extensions
     db.init_app(app)
-    migrate.init_app(app, db)
-
-    from models import (
-        TopFillMessage,
-        BottomFillMessage,
-        AvailMessage,
-        MotorMessage
-    )
-    from models import TopFillMessage, BottomFillMessage, AvailMessage, MotorMessage
+    with app.app_context():
+        db.create_all()
 
     # Import and register blueprints/routes
     from routes import main as main_blueprint
-
-    # Import and start MQTT
-    
-    import mqtt
     app.register_blueprint(main_blueprint)
 
-    
     return app
+
+
